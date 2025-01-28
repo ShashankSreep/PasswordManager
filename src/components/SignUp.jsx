b4f9e8d0262d9cd284f590532a9de35aa78053b7
Shashank Sreepathi <sreepsha1234@gmail.com>
2025-01-28 00:08:40 -0800

    API code for manager done

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -38,17 +38,6 @@ function SignUp() {
         setShowPass(prevState => !prevState)
     }
     
-    // For now, just add a dummy thing to test (No need for bcrypt)
-    const addNewMasterPassword = async () =>  {
-        try {
-            const ref = doc(db, "MasterPassword", "TEST_PASSWORD")
-            await setDoc(ref, {
-                masterPassword: "TEST_PASSWORD"
-            });
-        } catch (error) {
-            console.error("Error adding new master password: ", error);
-        }
-    }
 
 
 
@@ -67,8 +56,6 @@ function SignUp() {
             setUserName("");
             setPassword("");
             navigate("/login");
-            // Call addNewMasterPassword function
-            addNewMasterPassword(); // Check if this works
             console.log("Added ")
         } catch (error) {
             console.error("Error creating user: ", error);
@@ -81,7 +68,7 @@ function SignUp() {
             console.log("User signed in successfully!");
             //setSignedIn(true);
             setItem("loggedin", true); // Set the signedIn state in localStorage
-            navigate("/dashboard");
+            navigate("/masterpass");
         } catch (error) {
             console.error("Error signing in: ", error);
         }
@@ -123,30 +110,6 @@ function SignUp() {
                 )}
               </button>
             </div>
-           
-            <div className="relative w-64 mt-5">
-              <input
-                type={showMasterPass ? "text" : "password"} // Toggle between text and password
-                placeholder="Master Password"
-                className="w-full px-4 py-2 border-b border-white-300 text-white focus:outline-none"
-              />
-              <button 
-                type="button" 
-                className="absolute right-4 top-1/2 transform -translate-y-1/2 text-white cursor-pointer"
-                onClick={toggleMasterPass}
-              >
-                {showMasterPass ? (
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" viewBox="0 0 24 24" strokeWidth="2" className="feather feather-eye">
-                    <path d="M1 12s3-9 11-9 11 9 11 9-3 9-11 9-11-9-11-9zM12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"></path>
-                  </svg>
-                ) : (
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" viewBox="0 0 24 24" strokeWidth="2" className="feather feather-eye-off">
-                    <path d="M17 1l6 6M1 1l18 18M14.2 14a3 3 0 1 0 0-4.2m-4.4 0a3 3 0 1 0 0 4.2M12 5c-4 0-7 4-7 4s3 4 7 4 7-4 7-4-3-4-7-4z"></path>
-                  </svg>
-                )}
-              </button>
-            </div>
-            
             <div className="flex flex-col space-y-3 mt-8">
               <button className="px-25 py-2 bg-blue-700 hover:bg-blue-600 rounded-md text-white cursor-pointer" onClick={createUser}>Sign up</button>
               <button className="flex items-center justify-center gap-2 px-4 py-2 bg-white border rounded-md text-black shadow hover:bg-gray-100 cursor-pointer" onClick={googleSignIn}>
---
+++ src/config/handlePassword.js
@@ -0,0 +1,75 @@
+// This contains all of the API calls to handle the passwords database
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { doc, setDoc, getFirestore, addDoc, deleteDoc, updateDoc, getDoc } from "firebase/firestore";
+import { db } from "./firebase";
+
+
+export const addNewEntry = async (name, email, password, website_name) => {
+    try {
+        const ref = await setDoc(doc(db, "Passwords", name), {
+            Name: name, 
+            Email: email, 
+            Password: password, 
+            Website: website_name,
+            Favorite: false
+        });
+        console.log("New entry added successfully!");
+    } catch (error) {
+        console.error("Error adding new entry: ", error);
+    }
+}
+
+export const retrieveAllEntries = async () => {
+    try {
+        const querySnapshot = await getDoc(doc(db, "Passwords"));
+        console.log("All Entries: ", querySnapshot.data());
+        return querySnapshot.data();
+    } catch (error) {
+        console.error("Error retrieving all entries: ", error);
+    }
+}
+
+export const deleteEntry = async (name) => {
+    try {
+        await deleteDoc(doc(db, "Passwords", name));
+        console.log("Entry deleted successfully!");
+    } catch (error) {    
+        console.error("Error deleting entry: ", error);
+    }
+}
+
+export const updateFavorite = async (name, fav_bool) => {
+    try {
+        const ref = await updateDoc(doc(db, "Passwords", name), {
+            Favorite: fav_bool
+        });
+        console.log("Entry marked as favorite!");
+    } catch (error) {
+        console.error("Error marking entry as favorite: ", error);
+    }
+}
+
+export const updateEntry = async (name, email, password, website_name, favorite) => {
+    try {
+        const ref = await updateDoc(doc(db, "Passwords", name), {
+            Name: name, 
+            Email: email, 
+            Password: password, 
+            Website: website_name,
+            Favorite: favorite
+        });
+        console.log("Entry updated successfully!");
+    } catch (error) {
+        console.error("Error updating entry: ", error);
+    }
+}
+
+export const retrieveEntry = async (name) => {
+    try {
+        const querySnapshot = await getDoc(doc(db, "Passwords", name));
+        console.log("Entry: ", querySnapshot.data());
+        return querySnapshot.data();
+    } catch (error) {
+        console.error("Error retrieving entry: ", error);
+    }
+}
